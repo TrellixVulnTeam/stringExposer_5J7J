98fc7f5f4a88fd52d94c45254b04ba268f4fa41b
jplowri0 <jplow.tech@gmail.com>
2022-05-13 21:30:17 +1000

    search working

---
+++ test.py
@@ -0,0 +1,46 @@
+#https://www.geeksforgeeks.org/how-to-obtain-the-line-number-in-which-given-word-is-present-using-python/
+
+# READ FILE
+df = open("strings.txt")
+ 
+# read file
+read = df.read()
+ 
+# return cursor to
+# the beginning
+# of the file.
+df.seek(0)
+read
+
+# create empty list
+arr = []
+ 
+# count number of
+# lines in the file
+line = 1
+for word in read:
+    if word == '\n':
+        line += 1
+#print("Number of lines in file is: ", line)
+ 
+for i in range(line):
+    # readline() method,
+    # reads one line at
+    # a time
+    arr.append(df.readline())
+
+def findline(word):
+    for i in range(len(arr)):
+        if word in arr[i]:
+            #print(i+1)
+            lineNumber = i
+            #https://stackoverflow.com/questions/2081836/how-to-read-specific-lines-from-a-file-by-line-number
+            f=open('strings.txt')
+            lines=f.readlines()
+            print(lines[lineNumber])
+            
+
+#Now we want to print those line numbers of i+1
+
+ 
+findline("cat")
