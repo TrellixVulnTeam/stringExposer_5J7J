4ab67fb5238a92225c6a2b7bae3252705edcb0d4
jplow <jplow.tech@gmail.com>
2022-08-04 13:23:32 +1000

    ioc extraction functionality added

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # stringExposer
 
-This is to look through malware sting dumps to ID anything interesting. 
+## About
+This is to look through malware sting dumps to ID anything interesting. Windows API calls are mapped. With the discovered API calls, you can goto malapi.io and map them. 
+
+It will then extract any potential IoC's from the list of strings that can be used for further pivoting. These will be saved as a dataframe. 
+
+## Usage
+Save the floss output into the main directory of this project and call it strings.txt. 
+
+Then execute using python3 stringExposer.py
+
+Check the iocList.csv for complete output of IoCs. 
+
+
+
--- stringExposer.py
+++ stringExposer.py
@@ -1,3 +1,8 @@
+import io
+import pandas as pd
+from msticpy.transform import IoCExtract
+
+
 #https://www.geeksforgeeks.org/how-to-obtain-the-line-number-in-which-given-word-is-present-using-python/
 
 # READ FILE
@@ -30,15 +35,16 @@ for i in range(line):
     arr.append(df.readline())
 
 def findline(values):
+    #print(values)
     for i in range(len(arr)):        
         if values in arr[i]:
-            #print("The values is: "+values)
+            #print(values)
             #print(i+1)
             lineNumber = i
             #https://stackoverflow.com/questions/2081836/how-to-read-specific-lines-from-a-file-by-line-number
             f=open('strings.txt')
             lines=f.readlines()
-            print(lines[lineNumber])
+            print("[+] Possible "+malapicategory+" action by malware. Windows API call found on this line: "+lines[lineNumber])
             f.close()
             
 
@@ -46,12 +52,40 @@ def findline(values):
 
 #https://www.pythonpool.com/python-remove-newline-from-list/
 #Importing the text into a list. We also have to remove the \n from each line
-myList = open("words.txt").readlines()
-new_list = []
-for i in myList: #removing the \n and placing into new list. 
-    new_list.append(i.replace("\n", ""))
-for item in new_list: 
-    findline(item) 
+#myList = open("words.txt").readlines()
+#new_list = []
+#for i in myList: #removing the \n and placing into new list. 
+#    new_list.append(i.replace("\n", ""))
+#for item in new_list: 
+#    findline(item) 
 
+print("Searching for Windows API calls in strings file....\n")
 
+#Looping through windows api calls. And identifying the category of windows api as per malapi.io
+malapiList = ["Enumeration", "Injection", "Evasion", "Spying", "Internet", "Anti_Debugging", "Ransomware", "Helper"]
+for malapicategory in malapiList:
+    
+    
+#https://www.pythonpool.com/python-remove-newline-from-list/
+#Importing the text into a list. We also have to remove the \n from each line
+    myList = open("malapi"+malapicategory+".txt").readlines()
+    new_list = []
+    for i in myList: #removing the \n and placing into new list. 
+        new_list.append(i.replace("\n", ""))
+    for item in new_list:
+        findline(item)
+            
+print("\nNow Extracing any Potential IoCs .... \n")
 
+#Converting the txt file into a csv
+df = pd.read_csv('strings.txt')
+header_list = ['Strings']
+df.to_csv("strings.csv", header=header_list, index=0)
+
+#Extracting and returning any IOCs. Done using msticpy and inbuilt regex. 
+df2 = pd.read_csv('strings.csv')
+ioc_extractor = IoCExtract()
+ioc_df = ioc_extractor.extract(data = df2, columns = ['Strings'])
+print(ioc_df)
+
+ioc_df.to_csv("iocList.csv")
